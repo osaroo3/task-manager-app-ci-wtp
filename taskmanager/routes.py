26a77ff43ce55eb970ec1e44b15f32b5d9cef94b
Arukwe Obo-Eba <oboebafriday@gmail.com>
2024-02-18 07:24:18 +0000

    Add features to update database categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -12,6 +12,7 @@ def home():
 def categories():
     categories = list(Category.query.order_by(Category.category_name).all())
     return render_template("categories.html", categories=categories)
+    
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
@@ -21,3 +22,13 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
